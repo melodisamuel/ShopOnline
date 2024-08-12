ad1e6e36f8023ebfaf5d7dc4e47563d958915465
apple <apple@apples-MacBook-Pro-2.local>
2024-08-12 12:01:07 +0100

    Created usercontroller

---
+++ prisma/migrations/20240812103531_add_default_to_isactive_user_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `User` MODIFY `isActive` BOOLEAN NOT NULL DEFAULT true;
---
+++ src/features/controller/userController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { prisma }from "~/prisma"
+import { User } from "@prisma/client"
+
+class UserController {
+        public async createUser(req: Request, res: Response) {
+        const {
+            email, password, firstName, lastName, avatar
+        } = req.body;
+
+        // Insert to db
+        const newUser: User = await prisma.user.create({
+            data: {
+            email, password, firstName, lastName, avatar
+            }
+        })
+
+        res.status(201).json(newUser);
+    }
+}
+
+
+export const userController: UserController = new UserController();
