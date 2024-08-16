a3b3c023d3456ec76ad448b9ad5e122fd3e087bd
apple <apple@apples-MacBook-Pro-2.local>
2024-08-16 13:41:31 +0100

    Implementing Signp User Function

--- src/features/user/controller/authController.ts
+++ src/features/user/controller/authController.ts
@@ -1,12 +1,19 @@
 import { User } from "@prisma/client"
 import { prisma } from "~/prisma";
 import { NextFunction, Request, Response } from "express";
-import { authservice } from "~/services/authService";
+import { authService } from "~/services/authService";
 import { HTTP_STATUS } from "~/features/globals/constants/http";
+import { BadRequestException } from "~/features/globals/middleware/errorMiddleware";
 
  class AuthController {
     public async registerUser(req: Request, res: Response, next: NextFunction) {
-const accessToken = await authservice.addUser(req.body);
+
+        if (await authService.isEmailAlreadyExist(req.body.email)) {
+            next(new BadRequestException('Email must be unique'))
+            return;
+        }
+
+    const accessToken = await authService.addUser(req.body);
     
         res.status(HTTP_STATUS.CREATED).json({
             message: 'User registered succesfully!',
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,30 +4,40 @@ import jwt from 'jsonwebtoken';
 import { NextFunction, Request, Response } from "express";
 import dotenv from 'dotenv';
 import { access } from "fs";
+import { string } from "joi";
 dotenv.config();
 
 class Authservice {
 
     public async addUser(requestBody: any) {
-  
-   const {
-    email, password, firstName, lastName, avatar
-} = requestBody;
-
-   // Insert to db
-   const newUser: User = await prisma.user.create({
-    data : {
-    email, password, firstName, lastName, avatar
+        const { email, password, firstName, lastName, avatar } = requestBody;
+    
+        // Check if user's email is unique
+        const newUser: User = await prisma.user.create({
+            data: {
+                email, password, firstName, lastName, avatar
+            }
+        });
+    
+        // Create JWT
+        const payload = {
+            email, firstName, lastName, avatar, role: newUser.role
+        };
+        // Use a valid expiresIn value
+        const accessToken: string = jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: '1h' });
+    
+        return accessToken;
     }
-}) 
-// Create jwt
-const payload = {
-    email, firstName, lastName, avatar, role: newUser.role
-}
-const accessToken:string = jwt.sign(payload, process.env.JWT_SECRET!)
 
-    return  accessToken;
+    public async isEmailAlreadyExist(email:string) {
+         const userByEmail = await prisma.user.findFirst ({
+            where: {
+                email
+            }
+         })
+
+         return userByEmail !== null; 
     }
 }
 
-export const authservice: Authservice = new Authservice();
+export const authService: Authservice = new Authservice();
